af65ea7fb6c88ce9b20e4d0c9e9b4e42aefe6d9a
Alejandro Alfonso Fernández <alejandroalffer@users.noreply.github.com>
2020-06-03 07:23:53 +0200

    Prevent message error in update-nginx.sh

--- scripts/update-nginx.sh
+++ scripts/update-nginx.sh
@@ -4,7 +4,7 @@ set -e
 
 cp /root/alastria-access-point/nginx/conf.d/access-point.conf /etc/nginx/conf.d/access-point.conf
 touch /etc/nginx/conf.d/whitelist
-rm /etc/nginx/sites-enabled/default
+rm -f /etc/nginx/sites-enabled/default
 
 set +u
 set +e
